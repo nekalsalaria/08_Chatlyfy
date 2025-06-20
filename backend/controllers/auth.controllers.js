d941ef9bc2232b1db2cddf139e0f3deb97bd6436
NEKAL SINGH SALARIA <166684227+nekalsalaria@users.noreply.github.com>
2025-06-21 00:29:47 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -31,8 +31,8 @@ export const signup = async (req, res) => {
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"Lax",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
         return res.status(201).json(user)
@@ -61,8 +61,8 @@ export const Login = async (req, res) => {
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"Lax",
-            secure:false
+           sameSite:"None",
+            secure:true
         })
 
         return res.status(200).json(user)
